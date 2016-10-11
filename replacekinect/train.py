52206670cf59d6898f48417a12f171b374bcdb42
Md. Iftekhar Tanveer <go2chayan@gmail.com>
2016-10-11 16:40:21 -0400

    Bugfix in train.py

--- replacekinect/train.py
+++ replacekinect/train.py
@@ -18,22 +18,28 @@ def data_stream(datafile,datrng,batchsize=128):
         raise IOError('File not found')
     assert type(datrng) in {list,tuple} and len(datrng) == 2 and datrng[0]<=datrng[1]
     with h5py.File(datafile,'r') as f:
-        for subj in np.random.shuffle(f.keys()):
+        allsubj = f.keys()
+        np.random.shuffle(allsubj)
+        for subj in allsubj:
             if int(subj)>=datrng[0] and int(subj)<datrng[1]:
-                for vid in np.random.shuffle(f[subj].keys()):
+                allvids = f[subj].keys() 
+                np.random.shuffle(allvids)
+                for vid in allvids:
                     v = subj+'/'+vid+'/video_frames'
                     s = subj+'/'+vid+'/joints'
                     ind = 0
                     N = np.size(f[v],axis=0)
                     while ind<N:
                         end = min(ind+batchsize,N)
-                        i = np.random.shuffle(np.arrange(ind,end))
-                        yield f[v][i,:,:,:],f[s][i,:]
+                        i = np.arange(end-ind)
+                        np.random.shuffle(i)
+                        frames,joints = f[v][ind:end,:,:,:],f[s][ind:end,:]
+                        yield frames[i,:,:,:],joints[i,:]
                         ind+=batchsize
 
 # Vgg model without fully connected layer
 vggmodel = vgg16.VGG16(include_top=False)
-vggmodel.summary()
+#vggmodel.summary()
 
 # create fully connected layer
 fc_input = Input(shape=(512,5,10))
@@ -43,6 +49,7 @@ x = Dense(1024, activation='relu',name='fc2')(x)
 x = Dense(60,activation='linear',name='predictions')(x)
 fcmodel = Model(fc_input,x)
 fcmodel.compile(loss='mean_squared_error',optimizer='adagrad',metrics=['accuracy'])
+fcmodel.summary()
 print 'Fully-Connected model prepared'
 
 # Create batch and feed the fully connected neural network
@@ -51,19 +58,18 @@ test_stream = data_stream(datafile,testset,batchsize=1)
 print 'Starting Training ... '
 for iter in range(nb_iter):
     for frames, joints in data_stream(datafile,trainset):
-        print 'getting cnn activations ...'
         newinput = vggmodel.predict(frames)
-        print 'training FCN ...'
         tr_loss = fcmodel.train_on_batch(newinput,joints)
-        tst_loss = fcmodel.test_on_batch(*next(test_stream))
+        tst_frame,tst_joints = next(test_stream)
+        tst_frame = vggmodel.predict(tst_frame)
+        tst_loss = fcmodel.test_on_batch(tst_frame,tst_joints)
         count+=len(frames)
-        print '# of Data feed:',count, 'Mean Train Loss:',np.mean(tr_loss),
-        'Test Loss:',tst_loss
+        print '# of Data fed:',count, 'Mean Train Loss:',np.mean(tr_loss),\
+            'Test Loss:',tst_loss[0]
+    print 'saving weights ...',
+    fcmodel.save_weights('weightfile.h5')
+    print 'done.'
 print 'Training Finished'
-
-print 'saving weights ...',
-fcmodel.save_weights('weightfile.h5')
-print 'done.'
 
 # Create batch over test dataset and compute loss
 #print 'loss calculation'
