a2382235654c9fcefdb513aec2ae9ffcfbe519cc
Md. Iftekhar Tanveer <go2chayan@gmail.com>
2016-10-30 20:31:36 -0400

    Added some documentation

--- replacekinect/sample.sh
+++ replacekinect/sample.sh
@@ -1,4 +1,13 @@
 # Train the original preset model after loading the 
 # weights saved in previous iterations
 python train.py '/scratch/mtanveer/automanner_dataset.h5' --load_weights
+# Train the original preset model WITHOUT loading the 
+# weights saved in previous iterations
+python train.py '/scratch/mtanveer/automanner_dataset.h5'
+# Train the preset model with only 4 CNN blocks
+python train.py '/scratch/mtanveer/automanner_dataset.h5' -m 2
+# Train the preset model with only 3 CNN blocks
+python train.py '/scratch/mtanveer/automanner_dataset.h5' -m 3
+
+# Data file in my local folder structure
 # python train.py /Users/itanveer/Data/ROCSpeak_BL/allData_h5/automanner_dataset.h5 --load_weights
