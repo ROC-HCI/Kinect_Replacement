7bd6c9abda3784e56a598e2996f65d08895c89d1
Md. Iftekhar Tanveer <go2chayan@gmail.com>
2016-11-29 00:36:57 -0500

    custom model changed for quaternion

--- replacekinect/make_custom_model.py
+++ replacekinect/make_custom_model.py
@@ -10,7 +10,7 @@ from learningtools.preset_models import custom_vgg16, load_pretrained_weights
 # batch normalization layers. No regularizations
 
 # Filename prefix
-prefix = 'cnn4_tunable_dd_bn'
+prefix = 'cnn4_tunable_bn_quater'
 
 # VGG with first 4 conv blocks
 vggmodel = custom_vgg16(4)
@@ -26,23 +26,19 @@ x = Convolution2D(512, 3, 3, activation='relu', border_mode='same',\
  name='block5_conv3')(x)
 
 x = Flatten(name='flatten_fourcnn')(x)
-x = Dense(2048,name='fc1')(x)
+x = Dense(1024,name='fc1')(x)
 x = BatchNormalization(name='fc1_bn')(x)
 x = Activation('relu',name='fc1_relu')(x)
 
-x = Dense(2048,name='fc2')(x)
+x = Dense(1024,name='fc2')(x)
 x = BatchNormalization(name='fc2_bn')(x)
 x = Activation('relu',name='fc2_relu')(x)
 
-x = Dense(2048,name='fc3')(x)
+x = Dense(30,name='fc3')(x)
 x = BatchNormalization(name='fc3_bn')(x)
 x = Activation('relu',name='fc3_relu')(x)
 
-x = Dense(2048,name='fc4')(x)
-x = BatchNormalization(name='fc4_bn')(x)
-x = Activation('relu',name='fc4_relu')(x)
-
-x = Dense(60,activation='linear',name='predictions')(x)
+x = Dense(76,activation='linear',name='predictions')(x)
 fcmodel = Model(fc_input,x)
 
 # Compile and print summary
